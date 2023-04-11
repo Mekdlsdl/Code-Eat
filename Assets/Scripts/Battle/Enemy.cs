a09959ce07e03c5b236d2011f9c05b254977362e
kingharong <91714@naver.com>
2023-04-11 20:44:14 +0900

    하연 : 적 좌우 이동 무한 반복

--- Assets/Scripts/Battle/Enemy.cs
+++ Assets/Scripts/Battle/Enemy.cs
@@ -6,11 +6,22 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] public float hp { get; private set; } = 100;
     public bool isDead { get; private set; } = false;
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private Vector2 pos;
+    private float maxX = 6.0f;
+    [SerializeField] private float speed = 5.0f;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pos = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 v = pos;
+        v.x += maxX * Mathf.Sin(Time.time * speed);
+        transform.position = v;
     }
 
     private void OnTriggerEnter2D(Collider2D collision) {
