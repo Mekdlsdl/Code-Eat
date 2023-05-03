e17bc7c87fc68335737a3fa52bae4bdf32f2bdd1
tw9282 <tw9282@hufs.ac.kr>
2023-05-04 04:57:13 +0900

    최종 점수화면 및 랭킹
    동점자 처리 어떻게 할 건지 정하고 수정해야함

---
+++ Assets/Scripts/GameClear/Ranking.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Ranking : MonoBehaviour
+{
+    private TextMesh rankingTextUI;
+    private TextMesh rankingText;
+
+    public void UpdateRankings()
+    {
+        // 플레이어 점수 리스트 생성
+        List<int> playerScores = new List<int>();
+        foreach (PlayerConfiguration playerConfig in PlayerConfigManager.instance.PlayerConfigs)
+        {
+            playerScores.Add(playerConfig.PlayerScore);
+        }
+
+        // 내림차순 정렬
+        playerScores.Sort();
+        playerScores.Reverse();
+
+
+        // 랭킹을 보여주는 문자열 생성
+        string rankingString = "";
+        int rank = 1;
+        foreach (int score in playerScores)
+        {
+            if (rank > 10) break; // top10만 보여주도록 설정
+            foreach (PlayerConfiguration playerConfig in PlayerConfigManager.instance.PlayerConfigs)
+            {
+                if (playerConfig.PlayerScore == score)
+                {
+                    InsertRank(score);
+                    rankingString += $"{rank}. P{playerConfig.PlayerIndex + 1}: {score}\n";
+                    rank++;
+                    break;
+                }
+            }
+        }
+
+        rankingTextUI.text = rankingString;
+    }
+
+    public void InsertRank(int Score)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            if (Score > PlayerPrefs.GetInt(i.ToString()))
+            {
+                for (int j = 9 - i; j > 0; j--)
+                {
+                    PlayerPrefs.SetInt(j.ToString(), PlayerPrefs.GetInt((j - 1).ToString()));
+                }
+                PlayerPrefs.SetInt(i.ToString(), Score);
+                break;
+            }
+        }
+
+        rankingText.text = "Ranking\n\n" +
+            "1. " + PlayerPrefs.GetInt("0") + "\n\n" +
+            "2. " + PlayerPrefs.GetInt("1") + "\n\n" +
+            "3. " + PlayerPrefs.GetInt("2") + "\n\n" +
+            "4. " + PlayerPrefs.GetInt("3") + "\n\n" +
+            "5. " + PlayerPrefs.GetInt("4") + "\n\n" +
+            "6. " + PlayerPrefs.GetInt("5") + "\n\n" +
+            "7. " + PlayerPrefs.GetInt("6") + "\n\n" +
+            "8. " + PlayerPrefs.GetInt("7") + "\n\n" +
+            "9. " + PlayerPrefs.GetInt("8") + "\n\n" +
+            "10. " + PlayerPrefs.GetInt("9");
+    }
+
+    void Start()
+    {
+        rankingTextUI = FindObjectOfType<TextMesh>();
+        rankingText = GameObject.Find("RankingText").GetComponent<TextMesh>();
+
+        UpdateRankings();
+
+    }
+}
---
+++ Assets/Scripts/GameClear/Winner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class Winner : MonoBehaviour
+{
+    private TextMesh winnerTextUI;
+    private Text winnerText;
+
+    void UpdateWinner()
+    {
+       // 우승자 출력
+       PlayerConfiguration winner = GetWinner();
+       if (winner != null)
+       {
+           Debug.Log($"P {winner.PlayerIndex + 1} 이(가) 우승자입니다!");
+       }
+       else
+       {
+           Debug.Log("우승자가 없습니다.");
+       }
+   }
+
+   private PlayerConfiguration GetWinner()
+   {
+       List<PlayerConfiguration> players = PlayerConfigManager.instance.PlayerConfigs;
+
+       PlayerConfiguration winner = null;
+       int highestScore = int.MinValue;
+
+       foreach (PlayerConfiguration playerConfig in players)
+       {
+           int score = playerConfig.PlayerScore;
+           if (score > highestScore)
+           {
+               highestScore = score;
+               winner = playerConfig;
+           }
+       }
+
+       return winner;
+   }
+    void Start()
+    {
+        winnerText = winnerTextUI.transform.Find("WinnerText").GetComponent<Text>();
+
+        UpdateWinner();
+    }
+}
