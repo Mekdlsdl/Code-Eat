99895057e72455f3a4b5a42c60a72e9a65f15a5a
Siwan_201700746 <chwdqo@naver.com>
2023-05-09 20:38:41 +0900

    Merge branch 'main' of https://github.com/catnip144/AAA-Project

---
+++ Assets/Scripts/ProblemMode/Tree/TreeFillOrder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeFillOrder : MonoBehaviour
+{
+//     [SerializeField] private GameObject guide;
+//     [SerializeField] private GameObject guideContent;
+//     string guideText = "순회 방법을 보고 빈칸을 채워보세요!";
+
+//     void Start()
+//     {
+//         guideContent = transform.Find("Text");
+//         guideContent.text = guideText;
+//     }
+}
---
+++ Assets/Scripts/ProblemMode/Tree/TreeProblem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeProblem : MonoBehaviour
+{
+    [System.NonSerialized] public ProblemManager pm;
+    [SerializeField] private GameObject guide, tree;
+    [SerializeField] private List<bool> generateNode;
+
+    
+}
