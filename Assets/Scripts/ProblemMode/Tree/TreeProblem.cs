16f5b4e1e68b6a9e9acb8c8c6306aace0eae939c
Mekdlsdl <ekdlsdl0@hufs.ac.kr>
2023-05-17 16:48:59 +0900

    트리 문제 유형 1 완료
    ProblemTestMode 씬에서 테스트 가능!
    
    플레이어, 공격 로직 그대로 둬서 관련 오류 뜨는데 무시해도 됩니다!

--- Assets/Scripts/ProblemMode/Tree/TreeProblem.cs
+++ Assets/Scripts/ProblemMode/Tree/TreeProblem.cs
@@ -1,13 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TreeProblem : MonoBehaviour
 {
-    [System.NonSerialized] public ProblemManager pm;
-    [SerializeField] public GameObject guide, tree;
-    // [SerializeField] public TextMeshProUGUI guideContent;
-    [SerializeField] private List<bool> generateNode;
+    // [System.NonSerialized] public ProblemManager pm;
+    [SerializeField] public List<bool> generateNode;
+    [SerializeField] public List<GameObject> treeNode, treeName;
+    [SerializeField] public List<TNode> node = new List<TNode>();
 
-    
+
+    System.Random random = new System.Random();
+
+    void Start() {
+        RandomTree();
+        node = Tree();
+        Debug.Log(node.Count);
+    }
+
+    void RandomTree() {
+        List<bool> bools = new List<bool> {true, false};
+        generateNode = new List<bool>();
+
+        for (int i=0; i<treeNode.Count; i++) {
+            int ranBool = random.Next(2);
+            generateNode.Add(bools[ranBool]);
+        }
+
+        for (int j=0; j<treeNode.Count; j++) {
+            treeNode[j].SetActive(generateNode[j]);
+            treeName[j+3].SetActive(generateNode[j]);
+        }
+    }
+
+
+    /*
+        A -> B,C
+        B -> D,E
+        C -> F,G
+
+        tNode = {Now, Left, Right}
+    */
+
+    public class TNode {
+        public GameObject Now { get; set; }
+        public TNode Left { get; set; }
+        public TNode Right { get; set; }
+
+        public TNode(GameObject now, TNode left, TNode right) {
+            Now = now;
+            Left = left;
+            Right = right;
+        }
+    }
+
+    /*
+
+       A
+     B   C
+    D E F G
+
+    */
+
+    [SerializeField] public List<TNode> Tree() {
+        // treeName = new List<GameObject>();
+        // 역순으로 삽입
+        Debug.Log(treeName.Count);
+
+        if (treeName.Count == 7) {
+            // D, E, F, G
+            for (int j=6; j>2; j--) {
+                if (treeName[j].activeSelf) {
+                    TNode tNode = new TNode(treeName[j], null, null);
+                    node.Add(tNode);
+                } else {
+                    node.Add(null);
+                    // node = {G, F, E, D}인 상태
+                }
+            }
+
+            // C
+            TNode tNodeC = new TNode(treeName[2], node[1], node[0]);
+            node.Add(tNodeC);
+            // node = {G, F, E, D, C}인 상태
+
+            // B
+            TNode tNodeB = new TNode(treeName[1], node[3], node[2]);
+            node.Add(tNodeB);
+            // node = {G, F, E, D, C, B}인 상태
+
+            // A
+            TNode tNodeA = new TNode(treeName[0], node[5], node[4]);
+            node.Add(tNodeA);
+            // node = {G, F, E, D, C, B}인 상태
+        }
+
+        return node;
+    }
 }
--- Assets/Scripts/ProblemMode/Tree/TreeWhatOrder.cs
+++ Assets/Scripts/ProblemMode/Tree/TreeWhatOrder.cs
@@ -1,18 +1,196 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class TreeWhatOrder : MonoBehaviour
+public class TreeWhatOrder : TreeProblem
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject guide, tree, option, question;
+    [SerializeField] private List<TMP_Text> treeOptions;
+    // [SerializeField] private List<GameObject> questions;
+    private List<string> optionContent;
+    private List<int> optionIndex;
+    [SerializeField] public List<TNode> getNode;
+    private List<GameObject> orderResult;
+    [SerializeField] public TMP_Text resultText;
+    System.Random random = new System.Random();
+
+    /*
+        OrderOption() : 선택지 랜덤 생성
+        ShowResult() : 순회 결과 출력 및 트리에 표시
+        OrderMain() : 랜덤 종류의 순회 실행
+
+            PreOrder() : 전위순회
+            InOrder() : 중위순회
+            PostOrder() : 후위순회
+            LevelOrder() : 레벨순회
+    
+    */
+
     void Start()
     {
-        
+        StartCoroutine(BeginProblem());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator BeginProblem()
     {
-        
+        yield return new WaitForSeconds(2f);
+        guide.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        guide.SetActive(false);
+        yield return new WaitForSeconds(1f);
+        tree.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        OrderOption();
+        OrderMain();
+        question.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        option.SetActive(true);
+    }
+
+    void OrderOption() {
+        optionContent = new List<string> {"전위순회", "중위순회", "후위순회", "레벨순회"};
+        optionIndex = new List<int>();
+
+        while (true) {
+            int num = random.Next(optionContent.Count);
+
+            if (!optionIndex.Contains(num)) {
+                optionIndex.Add(num);
+            }
+
+            if (optionIndex.Count == optionContent.Count) {
+                break;
+            }
+        }
+
+        int j = 0;
+        foreach (string content in optionContent) {
+            treeOptions[optionIndex[j]].text = content;
+            j++;
+            if (j >= optionContent.Count) {
+                break;
+            }
+        }
+
+    }
+
+    IEnumerator ShowResult() {
+        for (int i=0; i<orderResult.Count; i++) {
+            GameObject res = question.transform.GetChild(i).gameObject;
+            Image resImage = res.GetComponent<Image>();
+            Image orderImage = orderResult[i].GetComponent<Image>();
+            Color ordColor = orderImage.color;
+            ordColor.a = 0.5f;
+
+            resImage.sprite = orderImage.sprite;
+            res.SetActive(true);
+            orderImage.color = ordColor;
+
+            yield return new WaitForSeconds(0.4f);
+            ordColor.a = 1;
+            orderImage.color = ordColor;
+        }
+    }
+
+    void OrderMain() {
+        TreeProblem tpScript = tree.GetComponent<TreeProblem>();
+        orderResult = new List<GameObject>();
+        getNode = tpScript.node;
+        Debug.Log(getNode.Count);
+
+        int last = getNode.Count - 1;
+        TNode root = getNode[last];
+
+        int orderNum = random.Next(4);
+        // int orderNum = 3; //테스트용
+
+        switch(orderNum) {
+            case 0:
+                Debug.Log("PreOrder");
+                PreOrder(root);
+                break;
+
+            case 1:
+                Debug.Log("InOrder");
+                InOrder(root);
+                break;
+            
+            case 2:
+                Debug.Log("PostOrder");
+                PostOrder(root);
+                break;
+
+            case 3:
+                Debug.Log("LevelOrder");
+                LevelOrder(getNode, root);
+                break;
+        }
+
+        int answerIndex = optionIndex[orderNum];
+        AnswerManager.instance.SetProblemAnswer(answerIndex);
+        Debug.Log($"정답 인덱스 : {(AnswerButton) answerIndex}");
+
+        Debug.Log(orderResult.Count);
+        StartCoroutine(ShowResult());
+    }
+
+    void PostOrder(TNode n) {
+        // Left - Right - Now
+
+        if (n.Left is not null) {
+            PostOrder(n.Left);
+        }
+        if (n.Right is not null) {
+            PostOrder(n.Right);
+        }
+        if (n.Now is not null) {
+            orderResult.Add(n.Now);
+        }
+    }
+
+    void InOrder(TNode n) {
+        // Left - Now - Right
+
+        if (n.Left is not null) {
+            InOrder(n.Left);
+        }
+        if (n.Now is not null) {
+            orderResult.Add(n.Now);
+        }
+        if (n.Right is not null) {
+            InOrder(n.Right);
+        }
+    }
+
+    void PreOrder(TNode n) {
+        // Now - Left - Right
+
+        if (n.Now is not null) {
+            orderResult.Add(n.Now);
+        }
+        if (n.Left is not null) {
+            PreOrder(n.Left);
+        }
+        if (n.Right is not null) {
+            PreOrder(n.Right);
+        }
+    }
+
+    void LevelOrder(List<TNode> getN, TNode n) {
+        // node 차례대로 출력
+
+        Debug.Log("getN" + getN.Count);
+        List<TNode> getNodeL = getN;
+        Debug.Log(getNodeL.Count);
+
+        int last = getNodeL.Count - 1;
+        for (int k=last; k>-1; k--) {
+            if (getNodeL[k] is not null) {
+                orderResult.Add(getNodeL[k].Now);
+            }
+        }
     }
 }
