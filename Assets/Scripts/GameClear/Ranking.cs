b939b08114ca057919547aaab060f47eb9b735de
skytower144 <127186795+catnip144@users.noreply.github.com>
2023-06-08 04:20:16 +0900

    리더보드 점수 표시 로직 수정
    * 이전 리더보드 기록이 사라지는 버그 수정
    
    * 리더보드 점수 표시 로직 수정

--- Assets/Scripts/GameClear/Ranking.cs
+++ Assets/Scripts/GameClear/Ranking.cs
@@ -6,72 +6,91 @@ using TMPro;
 
 public class Ranking : MonoBehaviour
 {
-    [SerializeField] private bool debug_rank;
+    [SerializeField] private bool isDebug = false;
+    private List<int> mergedScores;
     void OnEnable()
     {
-        if (debug_rank) ClearRank(); // 정확한 디버깅을 위해 임시로 추가
+        if (isDebug) ClearRank();
+        
         UpdateRankings();
-        DisplayRank();
     }
 
-    public void UpdateRankings()
+    private void UpdateRankings()
     {
-        // 플레이어 점수 리스트 생성
+        List<int> existingScores = new List<int>();
+        for (int i = 0; i < 10; i++)
+            existingScores.Add(PlayerPrefs.GetInt("score" + i, 0));
+        
         List<int> playerScores = new List<int>();
         foreach (PlayerConfiguration playerConfig in PlayerConfigManager.instance.PlayerConfigs)
-        {
             playerScores.Add(playerConfig.PlayerScore);
-        }
+        
+        // 이전 리더보드의 점수와 현재 플레이어들의 점수를 통합
+        List<int> mergedScores = existingScores.Concat(playerScores).ToList();
+        
+        // 중복되는 플레이어 점수 제거
+        int[] uniqueScoreArray = new SortedSet<int>(mergedScores).ToArray();
+        Array.Reverse(uniqueScoreArray);
 
-        // 각 플레이어의 점수를 InsertRank 메소드로 전달
-        foreach (int score in playerScores)
-        {
-            InsertRank(score);
-        }
-    }
+        // 표시할 점수 개수를 10개로 한정
+        Array.Resize(ref uniqueScoreArray, 10);
 
-    private void InsertRank(int score)
-    {
+        // 새롭게 갱신된 리더보드 상태 저장
         for (int i = 0; i < 10; i++)
-        {
-            if (score > PlayerPrefs.GetInt("score" + i, 0))
-            {
-                for (int j = 9 - i; j > 0; j--)
-                {
-                    PlayerPrefs.SetInt("score" + j, PlayerPrefs.GetInt("score" + (j - 1), 0));
-                }
-                PlayerPrefs.SetInt("score" + i, score);
-                PlayerPrefs.Save(); //갱신된 랭킹 저장
-                break;
-            }
-        }
+            PlayerPrefs.SetInt("score" + i, uniqueScoreArray[i]);
+        PlayerPrefs.Save();
+
+        // 리더보드 표시
+        DisplayRank(uniqueScoreArray);
     }
 
-    private void DisplayRank()
+    // private void UpdateRankings()
+    // {
+    //     // 플레이어 점수 리스트 생성
+    //     List<int> playerScores = new List<int>();
+    //     foreach (PlayerConfiguration playerConfig in PlayerConfigManager.instance.PlayerConfigs)
+    //     {
+    //         playerScores.Add(playerConfig.PlayerScore);
+    //     }
+
+    //     // 각 플레이어의 점수를 InsertRank 메소드로 전달
+    //     foreach (int score in playerScores)
+    //     {
+    //         InsertRank(score);
+    //     }
+    // }
+
+    // private void InsertRank(int score)
+    // {
+    //     for (int i = 0; i < 10; i++)
+    //     {
+    //         if (score > PlayerPrefs.GetInt("score" + i, 0))
+    //         {
+    //             for (int j = 9 - i; j > 0; j--)
+    //             {
+    //                 PlayerPrefs.SetInt("score" + j, PlayerPrefs.GetInt("score" + (j - 1), 0));
+    //             }
+    //             PlayerPrefs.SetInt("score" + i, score);
+    //             PlayerPrefs.Save(); //갱신된 랭킹 저장
+    //             break;
+    //         }
+    //     }
+    // }
+
+    private void DisplayRank(int[] scores)
     {
         TextMeshProUGUI[] ranks = GetComponentsInChildren<TextMeshProUGUI>();
 
-        int[] scoreArray = new int[10];
-        for (int i = 0; i < 10; i++)
-            scoreArray[i] = PlayerPrefs.GetInt("score" + i, 0);
-        
-        // 중복되는 플레이어 점수 제거
-        int[] uniqueScoreArray = new SortedSet<int>(scoreArray).ToArray();
-        Array.Reverse(uniqueScoreArray);
-
         for (int i = 0; i < 10; i++)
         {
-            int score = 0;
-            if (i < uniqueScoreArray.Length)
-                score = uniqueScoreArray[i];
-            
-            string textColor = (score == 0) ? "#BAB5B5" : "#A17171";   
-            ranks[i].text = $"{i + 1}. <color={textColor}>{score}pt</color>";
+            string textColor = (scores[i] == 0) ? "#BAB5B5" : "#A17171";   
+            ranks[i].text = $"{i + 1}. <color={textColor}>{scores[i]}pt</color>";
         }
     }
 
     private void ClearRank()
     {
+        Debug.Log("Clearing Ranks");
         for (int i = 0; i < 10; i++)
             PlayerPrefs.DeleteKey("score" + i);
     }
